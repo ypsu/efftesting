20486b22055a6b3948355b415b4430f2a4ff9dfb
Balazs Kezes <3j4b0y1i@anonaddy.me>
2024-08-09 07:04:38 +0200

    efftesting: add a dummy differ and fix a small detabbing issue

--- expect.go
+++ expect.go
@@ -30,9 +30,13 @@ func New(t any) ET {
 	return ET{t}
 }
 
-// detab removes the tab characters from the string.
+// detab removes the tab characters from the string if it's a multiline string.
+// That's because efftesting uses backticks for multiline strings and tab indents them.
 // Also remove the first newline character since that's how ` quoted strings start.
 func detab(s string) string {
+	if !strings.Contains(s, "\n") {
+		return s
+	}
 	return strings.TrimPrefix(strings.ReplaceAll(s, "\t", ""), "\n")
 }
 
@@ -43,13 +47,50 @@ func (et ET) Expect(desc string, got any, want expectationString) {
 	if g == w {
 		return
 	}
-	const format = "Non-empty diff for effect \"%s\"."
+	const format = "Non-empty diff for effect \"%s\", diff (-want, +got):\n%s"
+	diff := Diff(w, g)
 	switch reporter := et.t.(type) {
 	case errorfer:
-		reporter.Errorf(format, desc)
+		reporter.Errorf(format, desc, diff)
 	case printfer:
-		reporter.Printf(format, desc)
+		reporter.Printf(format, desc, diff)
 	default:
-		log.Printf(format, desc)
+		log.Printf(format, desc, diff)
 	}
+}
+
+// Context is the number of lines to display before and after the diff starts and ends.
+var Context = 2
+
+// Diff is the function to diff the expectation against the got value.
+// Defaults to a very simple diff treats all lines changed from the first until the last change.
+var Diff = dummydiff
+
+func dummydiff(lts, rts string) string {
+	if lts == rts {
+		return ""
+	}
+	lt, rt := strings.Split(lts, "\n"), strings.Split(rts, "\n")
+	minlen := min(len(lt), len(rt))
+	var commonStart, commonEnd int
+	for commonStart < minlen && lt[commonStart] == rt[commonStart] {
+		commonStart++
+	}
+	for commonEnd < minlen && lt[len(lt)-1-commonEnd] == rt[len(rt)-1-commonEnd] {
+		commonEnd++
+	}
+	d := make([]string, 0, 2*Context+len(lt)-commonStart-commonEnd+len(rt)-commonStart-commonEnd)
+	for i := max(0, commonStart-Context); i < commonStart; i++ {
+		d = append(d, " "+lt[i])
+	}
+	for i := commonStart; i < len(lt)-commonEnd; i++ {
+		d = append(d, "-"+lt[i])
+	}
+	for i := commonStart; i < len(rt)-commonEnd; i++ {
+		d = append(d, "+"+rt[i])
+	}
+	for i := len(lt) - commonEnd; i < min(len(lt), len(lt)-commonEnd+Context); i++ {
+		d = append(d, " "+lt[i])
+	}
+	return strings.Join(d, "\n") + "\n"
 }
--- expect_test.go
+++ expect_test.go
@@ -13,4 +13,15 @@ func TestExpectOK(t *testing.T) {
 	et.Expect("int1", 0, "0")
 	et.Expect("int2", -43, "-43")
 	et.Expect("string", "blah", "blah")
+	et.Expect("multiline1_quoted", "hello\nworld\n", "hello\nworld\n")
+	et.Expect("multiline2_quoted", "\nhello\nworld\n", "\n\nhello\nworld\n")
+	et.Expect("multiline1_backticked", "hello\nworld\n", `
+		hello
+		world
+	`)
+	et.Expect("multiline2_backticked", "\nhello\nworld\n", `
+
+		hello
+		world
+	`)
 }
